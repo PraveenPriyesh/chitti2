e78ead57794d2ce9025197b230ce7239762f86d3
PraveenPriyesh <129766701+PraveenPriyesh@users.noreply.github.com>
2024-10-07 07:28:09 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import ChatBot from './components/bot'
+function App() {
+  return (
+    <div className="App">
+      <ChatBot/>
+    </div>
+  );
+}
+
+export default App;
